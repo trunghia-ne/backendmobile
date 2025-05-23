0b76f9e2de9598297257c90bca09a7938f590a4e
trunghia-ne <22130182@st.hcmuaf.edu.vn>
2025-05-23 23:06:08 +0700

    create

---
+++ src/main/java/org/example/mobilebackendjava/MobileBackEndJavaApplication.java
@@ -0,0 +1,13 @@
+package org.example.mobilebackendjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MobileBackEndJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MobileBackEndJavaApplication.class, args);
+    }
+
+}
