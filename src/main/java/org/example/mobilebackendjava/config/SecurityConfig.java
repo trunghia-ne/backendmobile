c9c0408e4ccc91018984958d7e248cccfda6dc24
DanhPhuongNha-22130191 <22130191@st.hcmuaf.edu.vn>
2025-05-28 16:16:16 +0700

    Cấu hình thành công kết nối giữa Spring Boot và Cloude Firestore.

--- src/main/java/org/example/mobilebackendjava/config/SecurityConfig.java
+++ src/main/java/org/example/mobilebackendjava/config/SecurityConfig.java
@@ -1,4 +1,4 @@
-package org.example.mobilebackendjava;
+package org.example.mobilebackendjava.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
