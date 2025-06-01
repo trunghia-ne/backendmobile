18e92df18f8ed83400531e652c1cbe5f7c504608
Phat <22130206@st.hcmuaf.edu.vn>
2025-06-02 00:47:05 +0700

    deploy final

--- src/main/java/org/example/mobilebackendjava/config/FirebaseConfig.java
+++ src/main/java/org/example/mobilebackendjava/config/FirebaseConfig.java
@@ -1,35 +1,41 @@
 package org.example.mobilebackendjava.config;
 
 import com.google.auth.oauth2.GoogleCredentials;
-import com.google.cloud.firestore.Firestore;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
-import com.google.firebase.cloud.FirestoreClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import java.io.FileInputStream;
-import java.io.IOException;
+import java.io.*;
+import java.nio.charset.StandardCharsets;
 
 @Configuration
 public class FirebaseConfig {
 
-   @Bean
+    @Bean
     public FirebaseApp initFirebase() throws IOException {
-        FileInputStream serviceAccount =
-                new FileInputStream("src/main/resources/movieapp-f0c63-firebase-adminsdk-fbsvc-694814d465.json");
+        FirebaseOptions options;
 
-        FirebaseOptions options = new FirebaseOptions.Builder()
-                .setCredentials(GoogleCredentials.fromStream(serviceAccount)) // Kết nối với Cloude Firestore
-                .build();
+        String firebaseConfigEnv = System.getenv("FIREBASE_CONFIG");
+
+        if (firebaseConfigEnv != null && !firebaseConfigEnv.isEmpty()) {
+            // ✅ Ưu tiên chạy trên Render (đọc từ ENV)
+            ByteArrayInputStream serviceAccount = new ByteArrayInputStream(firebaseConfigEnv.getBytes(StandardCharsets.UTF_8));
+            options = FirebaseOptions.builder()
+                    .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                    .build();
+        } else {
+            // ✅ Fallback local: đọc file JSON trong src/main/resources
+            InputStream serviceAccount = new FileInputStream("src/main/resources/movieapp-f0c63-firebase-adminsdk-fbsvc-694814d465.json");
+            options = FirebaseOptions.builder()
+                    .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                    .build();
+        }
+
         if (FirebaseApp.getApps().isEmpty()) {
             FirebaseApp.initializeApp(options);
         }
-        return FirebaseApp.getInstance();
-    }
 
-    @Bean
-    public Firestore getFirestore(FirebaseApp firebaseApp) {
-        return FirestoreClient.getFirestore(firebaseApp);
+        return FirebaseApp.getInstance();
     }
 }
