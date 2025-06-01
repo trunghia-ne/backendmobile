fe34b26c2fae7260cfe69fa43c4d8785f381f86e
Phat <22130206@st.hcmuaf.edu.vn>
2025-06-02 00:53:32 +0700

    lan nay la final thiet ne

--- src/main/java/org/example/mobilebackendjava/config/FirebaseConfig.java
+++ src/main/java/org/example/mobilebackendjava/config/FirebaseConfig.java
@@ -1,8 +1,10 @@
 package org.example.mobilebackendjava.config;
 
 import com.google.auth.oauth2.GoogleCredentials;
+import com.google.cloud.firestore.Firestore;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
+import com.google.firebase.cloud.FirestoreClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -37,5 +39,10 @@ public class FirebaseConfig {
         }
 
         return FirebaseApp.getInstance();
+    }
+
+    @Bean
+    public Firestore getFirestore(FirebaseApp firebaseApp) {
+        return FirestoreClient.getFirestore(firebaseApp);
     }
 }
