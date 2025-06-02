fa4b3b2219569dbc76cc6f472f738634d73da56e
DanhPhuongNha-22130191 <22130191@st.hcmuaf.edu.vn>
2025-06-03 00:42:49 +0700

    Xóa back-end liên quan profile và lưu tt lịch sử phim

--- src/main/java/org/example/mobilebackendjava/model/Movie.java
+++ src/main/java/org/example/mobilebackendjava/model/Movie.java
@@ -1,16 +1,14 @@
 package org.example.mobilebackendjava.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Movie {
     private String name;
     private String slug;
     private String poster_url;
-
-    public String getName() { return name; }
-    public void setName(String name) { this.name = name; }
-
-    public String getSlug() { return slug; }
-    public void setSlug(String slug) { this.slug = slug; }
-
-    public String getPoster_url() { return poster_url; }
-    public void setPoster_url(String poster_url) { this.poster_url = poster_url; }
 }
